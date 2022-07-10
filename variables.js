71eaa602af70848c7b801c5e84f7d712c4ebcf7f
fernando <fernando.cr100888@gmail.com>
2022-07-10 07:36:39 -0400

    variables

---
+++ variables.js
@@ -0,0 +1,23 @@
+// ejemplo de variable
+var name = "Fernando"; 
+
+//------------------------------------
+// existen dos estados de una variable
+// 1) declarar
+var edad;
+
+// 2) inicializar
+edad = 30;
+
+//------------------------------------------------
+// también podemos generar variables más complejas
+var elementos = ["computadora", "celular"];
+
+var persona = {
+    nombre: "Fernando",
+    edad: 33
+}
+
+
+
+

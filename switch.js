0ccb28fa7cd528f3f816caa85e6cacff887d9122
fernando <fernando.cr100888@gmail.com>
2022-07-27 21:58:50 -0400

    switch y piedra, papel o tijera con switch

---
+++ switch.js
@@ -0,0 +1,15 @@
+var numero = 100;
+
+switch (numero) {
+    case 1:
+        console.log ("soy un 1")
+        break;
+    case 10:
+        console.log ("soy un 10")
+        break;
+    case 100:
+        console.log ("soy un 100")
+        break;
+    default:
+        console.log ("no soy nada")
+}
